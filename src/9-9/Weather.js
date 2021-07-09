f10821cd0af42a1631461a2ecf90ffc833101780
NikolasT1503 <Nikolas_t@mail.ru>
2021-07-09 22:28:28 +0300

    changes 9-9

--- src/9-9/Weather.js
+++ src/9-9/Weather.js
@@ -4,12 +4,22 @@ import FormWeather from './FormWeather';
 const API_KEY = '8317b8b85ce7e09eeb92e1f5b751e1e9';
 
 class Weather extends Component {
+    state = {
+        city: '',
+        temp: null,
+        isWeather: false
+    }
     gettingWeather = async (e) => {
         e.preventDefault();
-        const city = e.target.elements.city.value;
+        const city = e.target.elements.city.value
         const api_url = await
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
         const data = await api_url.json();
+        this.setState({
+            city: city,
+            temp: data.main.temp,
+            isWeather: true
+        })
         console.log(data)
     }
 
@@ -17,6 +27,8 @@ class Weather extends Component {
         return (
             <div>
                 <FormWeather weatherMethod={this.gettingWeather}/>
+                {this.state.isWeather && <p>Погода в городе {this.state.city}: {this.state.temp}</p>}
+                
             </div>
         );
     }
